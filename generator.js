97fa44fc462c493bb45e7ea213883260cc1fa828
goN <contato@arielton.com>
2022-06-29 10:11:15 -0300

    Create generator.js

---
+++ generator.js
@@ -0,0 +1,11 @@
+const generateRandomPassword = () => {
+    return Array(15) //number of characters
+      .fill(
+        "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      )
+      .map(function (x) {
+        const pwd = Math.floor(Math.random() * x.length);
+        return x.substring(pwd, pwd + 1);
+      })
+      .join("");
+  };
